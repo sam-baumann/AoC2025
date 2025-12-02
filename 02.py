65b276ae1c7cb2fc7ae3896956d930517deb68e8
Sam Baumann <sam.baumann@aai-tenn.com>
2025-12-02 14:45:25 -0500

    02 improvements

--- 02.py
+++ 02.py
@@ -8,29 +8,24 @@ ranges = [(int(id.split('-')[0]), int(id.split('-')[1]) + 1) for id in input.spl
 
 import re
 pt1_regex = re.compile(r'(.+)\1')
-def is_invalid(id):
-    if pt1_regex.fullmatch(id):
-        return True
-    else:
-        return False
-
 pt2_regex = re.compile(r'(.+)\1+')
 
-def pt2_is_invalid(id):
-    if pt2_regex.fullmatch(id):
+def is_invalid(id, regex):
+    if regex.fullmatch(id):
         return True
     else:
         return False
 
-invalid_ranges = 0
+
+pt1_ans = 0
 pt2_ans = 0
 for range_list in ranges:
     for i in range(*range_list):
-        if is_invalid(str(i)):
-            invalid_ranges += i
-            print(f'invalid: {i}')
-        if pt2_is_invalid(str(i)):
+        str_i = str(i)
+        if is_invalid(str_i, pt1_regex):
+            pt1_ans += i
+        if is_invalid(str_i, pt2_regex):
             pt2_ans += i
 
-print(invalid_ranges)
+print(pt1_ans)
 print(pt2_ans)
