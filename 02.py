23f7c9b1dd8794224d97c9169847d9a261c778fb
Sam Baumann <sam.baumann@aai-tenn.com>
2025-12-02 11:49:00 -0500

    day 2 pt 2 complete

--- 02.py
+++ 02.py
@@ -6,20 +6,31 @@ input = lines[0]
 
 ranges = [(int(id.split('-')[0]), int(id.split('-')[1]) + 1) for id in input.split(',')]
 
+import re
+pt1_regex = re.compile(r'(.+)\1')
 def is_invalid(id):
-    len_is_even = (len(id) % 2) == 0
-    if len_is_even:
-        id_half = len(id) // 2
-        if id[:id_half] == id[id_half:]:
-            return True
+    if pt1_regex.fullmatch(id):
+        return True
+    else:
+        return False
+
+pt2_regex = re.compile(r'(.+)\1+')
+
+def pt2_is_invalid(id):
+    if pt2_regex.fullmatch(id):
+        return True
     else:
         return False
 
 invalid_ranges = 0
+pt2_ans = 0
 for range_list in ranges:
     for i in range(*range_list):
         if is_invalid(str(i)):
             invalid_ranges += i
             print(f'invalid: {i}')
+        if pt2_is_invalid(str(i)):
+            pt2_ans += i
 
 print(invalid_ranges)
+print(pt2_ans)
