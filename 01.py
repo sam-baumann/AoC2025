db5abf088ef3d5556084b14e81f12b8b6bb49593
Sam Baumann <sam.k.baumann@gmail.com>
2025-12-02 07:09:15 -0500

    part 1 done

--- 01.py
+++ 01.py
@@ -4,6 +4,7 @@ with open("inputs/01.txt") as f:
 
 dial = 50
 total_zeros = 0
+pt2 = 0
 
 for line in lines:
     dir = line[0]
@@ -11,10 +12,20 @@ for line in lines:
 
     mod = -1 if dir == 'L' else 1
 
+    pt2_dial = dial
     dial += number * mod
+
     dial %= 100
 
     if dial == 0:
         total_zeros += 1
 
+    for num in range(number):
+        pt2_dial += 1 * mod
+        pt2_dial %= 100
+
+        if pt2_dial == 0:
+            pt2 += 1
+
 print(total_zeros)
+print(pt2)
