9d2304a7322ad8fd8bcad5cdfb92a4956c3e843f
Sam Baumann <sam.k.baumann@gmail.com>
2025-12-09 21:54:40 -0500

    day 8 complete

--- 08.py
+++ 08.py
@@ -1,3 +1,6 @@
+from math import sqrt, prod
+
+
 # disjoint set data structure should help with this problem
 class DisjointSet:
     """
@@ -29,4 +32,58 @@ class DisjointSet:
         x.size += y.size
 
     def __repr__(self) -> str:
-        return self.id
+        if self is self.parent:
+            return f"id: {self.id}"
+        return f"id: {self.id}, parent: {self.find()}"
+
+
+with open("inputs/08.txt") as f:
+    lines = [x.rstrip() for x in f.readlines()]
+
+
+def euclidian_distance(pt1, pt2):
+    pt1 = [int(x) for x in pt1.split(",")]
+    pt2 = [int(x) for x in pt2.split(",")]
+    diff = [x - y for x, y in zip(pt1, pt2)]
+    squared = sum([x**2 for x in diff])
+    return sqrt(squared)
+
+
+forest = {x: DisjointSet(x) for x in lines}
+
+distances = {}
+# find the distance between each point
+for i, line1 in enumerate(lines):
+    for line2 in lines[i + 1 :]:
+        distance = euclidian_distance(line1, line2)
+        distances[distance] = distances.get(distance, []) + [(line1, line2)]
+
+ordered_pairs = [
+    item for x in sorted(distances.items(), key=lambda x: x[0]) for item in x[1]
+]
+
+limit = 1000
+for pair in ordered_pairs[:limit]:
+    forest[pair[0]].union(forest[pair[1]])
+
+# get the size of every disjoint set
+sizes = {}
+for line in lines:
+    cur = forest[line].find()
+    sizes[cur] = cur.size
+
+sizes = [x[1] for x in sorted(sizes.items(), key=lambda x: x[1], reverse=True)]
+topn = 3
+print(sizes)
+print(prod(sizes[:topn]))
+
+# now we continue until all boxes are connected
+goal_size = len(lines)
+for pair in ordered_pairs[limit:]:
+    forest[pair[0]].union(forest[pair[1]])
+    if forest[pair[0]].find().size >= goal_size:
+        # get x coords
+        ans = int(pair[0].split(",")[0]) * int(pair[1].split(",")[0])
+        print(pair)
+        print(ans)
+        break
